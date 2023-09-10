696a345ddb4664a40b2dd036ca48da01a75674d4
Jacob Kueck <jacob.kueck@gmail.com>
2023-09-10 14:22:24 -0500

    added Javascript to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,13 @@
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+for (var x = 0; x < topics.length; x++)
+{console.log(topics[x]);}
+/*
+if (topics === 'HTML') {console.log("lets study HTML");
+} else if (topics === 'CSS') {console.log("lets study CSS!");
+} else if (topics === 'git') {console.log("lets study Git!");
+} else if (topics === 'javascript') {console.log("lets study Javascript");
+} else {console.log('please try again!');}
+var shapes = ["traingle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) 
+{console.log(shapes[x]);}
+*/
